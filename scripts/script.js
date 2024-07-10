1d335fd2d7d6e86e862bbe134ea57b79be21dbac
mohammad reza <mrghkh12@gmail.com>
2024-07-10 10:18:35 +0330

    feat(script.js): set actions for tasks

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,12 @@ function addTask(){
 
     inputBox.value = ''
 }
+
+listContainer.addEventListener('click' , e =>{
+    if(e.target.tagName == 'LI'){
+        e.target.classList.toggle('checked')
+    }
+    else if (e.target.tagName == "SPAN"){
+        e.target.parentElement.remove()
+    }
+})
